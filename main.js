e4d978ecdaea01cd10bdd4fbc2bfa82960ad81d5
Bob Brown <robertbernardbrown@gmail.com>
2019-02-18 19:47:36 -0500

    adds initial code for blockchain and block classes and gets a bsic version up

---
+++ main.js
@@ -0,0 +1,41 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = ''
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, new Date(), 'Genesis Block', '0')
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let blockchain = new Blockchain();
+blockchain.addBlock(new Block(1, '10/07/2019', { amount: 7 }))
+blockchain.addBlock(new Block(2, '11/07/2019', { amount: 10 }))
+
+console.log(JSON.stringify(blockchain, null, 4));
