bc019a1ed0eaa03b5dde97a855fdbed238283154
Bob Brown <robertbernardbrown@gmail.com>
2019-02-23 11:48:23 -0500

    adds mining proof of work to block chain

--- main.js
+++ main.js
@@ -5,21 +5,31 @@ class Block {
     this.index = index;
     this.timestamp = new Date();
     this.data = data;
-    this.hash = ''
+    this.hash = '';
+    this.nonce = 0;
   }
 
   calculateHash() {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined: ", this.hash);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock() {
-    return new Block(0, 'Genesis Block')
+    return new Block(0, 'Genesis Block');
   }
 
   getLatestBlock() {
@@ -28,7 +38,7 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     newBlock.index = this.chain.length;
     this.chain.push(newBlock);
   }
@@ -52,17 +62,24 @@ class Blockchain {
 }
 
 let blockchain = new Blockchain();
+console.log("Mining block 1...")
 blockchain.addBlock(new Block(1, { amount: 7 }))
+console.log("Mining block 2...")
 blockchain.addBlock(new Block(2, { amount: 10 }))
-console.log('Is it valid: ', blockchain.isChainValid());
-blockchain.addBlock(new Block(3, { amount: 10 }))
-blockchain.addBlock(new Block(4, { amount: 10 }))
-blockchain.chain[1].data = 300;
-blockchain.chain[1].hash = blockchain.chain[1].calculateHash();
-blockchain.chain[2].hash = blockchain.chain[2].calculateHash();
-blockchain.chain[3].hash = blockchain.chain[3].calculateHash();
-blockchain.chain[4].hash = blockchain.chain[4].calculateHash();
-console.log('Is it valid: ', blockchain.isChainValid());
+
+// tampering with blockchain to create valid chain after tampering with a block
+// let blockchain = new Blockchain();
+// blockchain.addBlock(new Block(1, { amount: 7 }))
+// blockchain.addBlock(new Block(2, { amount: 10 }))
+// console.log('Is it valid: ', blockchain.isChainValid());
+// blockchain.addBlock(new Block(3, { amount: 10 }))
+// blockchain.addBlock(new Block(4, { amount: 10 }))
+// blockchain.chain[1].data = 300;
+// blockchain.chain[1].hash = blockchain.chain[1].calculateHash();
+// blockchain.chain[2].hash = blockchain.chain[2].calculateHash();
+// blockchain.chain[3].hash = blockchain.chain[3].calculateHash();
+// blockchain.chain[4].hash = blockchain.chain[4].calculateHash();
+// console.log('Is it valid: ', blockchain.isChainValid());
 
 
 // console.log(JSON.stringify(blockchain, null, 4));
